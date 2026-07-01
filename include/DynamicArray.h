22478800960fba341c4f5c40a692d6d8d4aab8c4
Mayank Khanna <mayank@cqst.in>
2026-07-01 10:32:26 +0530

    implement basic DynamicArray with custom allocator

---
+++ include/DynamicArray.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <stdexcept>
+#include <utility>
+#include "Allocator.h"
+
+template <typename T>
+class DynamicArray{
+private:
+    T* data;
+    size_t size;
+    size_t capacity;
+    MyAllocator<T> allocator;
+public:
+    DynamicArray(){
+        size=0;
+        capacity=1;
+        data = allocator.Allocate(capacity);
+    }
+
+    DynamicArray(size_t n){
+        size=n;
+        capacity=n;
+        data = allocator.Allocate(capacity);
+        for(size_t i=0;i<n;i++){
+            allocator.Construct(data+i);
+        }
+    }
+
+    ~DynamicArray(){
+        for(size_t i=0;i<size;i++){
+            allocator.Destroy(data+i);
+        }
+        allocator.Deallocate(data);
+    }
+};
